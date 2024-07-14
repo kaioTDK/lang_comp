7a9a66937af22623c2b4bb9d12ae9089038431d7
kaio s. <kaiojugno@gmail.com>
2024-07-14 20:25:19 -0300

    csharp and updates

---
+++ csharp-prime/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace prime;
+using static System.Console;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool is_prime(uint number) {
+            for (int i = 1; i < number;i++){
+                if (number % i == 0 & i != 1) return false;
+            }
+            return true;
+        }
+
+        void is_prime_range(uint number) {
+            for (uint i = 1; i <= number;i++) {
+                bool prime = is_prime(i);
+            }
+        }
+        var time = new Stopwatch();
+        time.Start();
+        is_prime_range(100_000);
+        time.Stop();
+        WriteLine(time.ElapsedMilliseconds);
+        
+    }
+}
