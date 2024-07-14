44c88e935238f2e76826bc2da14afc94ea75d6b1
kaio s. <kaiojugno@gmail.com>
2024-07-14 18:09:15 -0300

    update lua

--- lua-prime/main_prime.lua
+++ lua-prime/main_prime.lua
@@ -1,1 +1,21 @@
-print("hello")
+function is_prime(number)
+    for i = 2, number -1, 1 do
+        if number % i == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+function is_prime_range(number)
+    for i = 1, number, 1 do
+        if is_prime(i) then
+            print(i," is prime!")
+        end
+
+    end
+    
+end
+
+-- print(is_prime(17),"\n")
+is_prime_range(10000)
