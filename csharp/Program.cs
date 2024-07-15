f86dc570ada2b9013a37d7f0ac741aab4324a563
kaio santos <kyoteru@pop-os.localdomain>
2024-07-15 18:41:13 -0300

    updade csharp

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,14 +1,10 @@
 ﻿namespace prime;
 using static System.Console;
 using System.Diagnostics;
-using BenchmarkDotNet.Running;
 class Program
 {
-    static void Main(string[] args)
-    {
-        BenchmarkRunner.Run<Benchmark>();
+    static void Main(string[] args){
 
-       return;
         bool is_prime(uint number) {
             for (int i = 2 ; i < number;i++){
                 if (number % i == 0 & i != 1 | number % 2 == 0 | number % 3 == 0) return false;
@@ -21,9 +17,22 @@ class Program
                 bool prime = is_prime(i);
             }
         }
+        void is_prime_range2(uint number) {
+            for (uint i = 1; i <= number;i++) {
+                
+                 for (int i = 2 ; i < number;i++){
+                if (number % i == 0 & i != 1 | number % 2 == 0 | number % 3 == 0) return;
+                }
+                Writeline($"{i} is prime!");
+            }
+        }
         var time = new Stopwatch();
         time.Start();
         is_prime_range(100_000);
+        time.Stop();
+        WriteLine(time.Elapsed);
+         time.Start();
+        is_prime_range2(100_000);
         time.Stop();
         WriteLine(time.Elapsed);
         
